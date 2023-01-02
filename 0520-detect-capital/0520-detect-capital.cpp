3694be403ac154c2a0cc0f3b302842395f30f3d9
Priyanshu Patel <68877780+Priyanshu-bit@users.noreply.github.com>
2023-01-02 20:50:09 +0530

    Time: 5 ms (24.37%), Space: 6.2 MB (24.57%) - LeetHub

---
+++ 0520-detect-capital/0520-detect-capital.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    bool detectCapitalUse(string word) {
+        
+        int count =0;
+        for (int i=0;i<word.size();i++){
+            if (isupper(word[i])){
+                count ++;
+            }
+        }
+        if (count ==word.size()||count ==1 &&isupper(word[0])||count==0){
+            return true;
+            
+        }
+        return false;
+        
+    }
+};
