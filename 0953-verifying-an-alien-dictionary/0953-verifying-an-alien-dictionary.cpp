850e80da22ae2a157ddc16528e7b02dfee603b46
Priyanshu Patel <68877780+Priyanshu-bit@users.noreply.github.com>
2023-02-02 20:20:00 +0530

    Time: 22 ms (5.61%), Space: 12.6 MB (5.21%) - LeetHub

---
+++ 0953-verifying-an-alien-dictionary/0953-verifying-an-alien-dictionary.cpp
@@ -0,0 +1,40 @@
+
+
+map<char, int> sequence;
+bool cmp(string a, string b)
+{
+    int n = min(a.length(), b.length());
+    for (int i = 0; i < n; i++)
+    {
+        if (sequence[a[i]] > sequence[b[i]])
+        {
+            return false;
+        }
+        else if (sequence[a[i]] < sequence[b[i]])
+        {
+            return true;
+        }
+    }
+    if (a.length() <= b.length())
+    {
+        return true;
+    }
+    return false;
+}
+
+
+class Solution {
+public:
+    bool isAlienSorted(vector<string> &words, string order)
+{
+    for (int i = 1; i <= 26; i++)
+    {
+        sequence[order[i - 1]] = i;
+    }
+    vector<string> temp = words;
+    sort(temp.begin(), temp.end(), cmp);
+    if (temp == words)
+        return true;
+    return false;
+    }
+};
