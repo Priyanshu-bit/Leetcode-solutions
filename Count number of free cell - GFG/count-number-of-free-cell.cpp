237fd0bee424dc4ebceac88c9b5f6de35b160f2b
Priyanshu Patel <68877780+Priyanshu-bit@users.noreply.github.com>
2023-02-08 21:46:55 +0530

    Added solution - LeetHub

---
+++ Count number of free cell - GFG/count-number-of-free-cell.cpp
@@ -0,0 +1,68 @@
+//{ Driver Code Starts
+//Initial Template for C++
+
+#include<bits/stdc++.h>
+using namespace std;
+
+
+
+// } Driver Code Ends
+//User function Template for C++
+
+
+class Solution{
+  public:
+  vector<long long int> countZero(int n, int k, vector<vector<int>>& arr){
+      //Code Here
+      vector<long long int >ans;
+      long long int fcell = n*n;
+     int colc=0;
+     int rowc=0;
+     vector <int >r(n,0),c(n,0);
+     for (int i=0;i<k;i++){
+         int row = arr[i][0]-1;
+         int col = arr[i][1]-1;
+         if (c[col]==0){
+             rowc++;
+             c[col]=1;
+            fcell= fcell-n+colc;
+             
+         }
+          if (r[row]==0){
+           colc++;
+             r[row]=1;
+            fcell= fcell-n+rowc;
+     }
+     ans.push_back(fcell);
+  }
+  return ans;}
+};
+
+//{ Driver Code Starts.
+int main(){
+    int t;
+    cin>>t;
+    while(t--){
+        int n,k;
+        cin>>n>>k;
+        vector<vector<int>> arr(k,vector<int>(2));
+        for(int i=0;i<k;i++){
+            int x, y;
+            cin>>x>>y;
+            arr[i][0] = x;
+            arr[i][1] = y;
+        }
+        Solution ob;
+        vector<long long int> ans = ob.countZero(n,k,arr);
+        
+        for(int i=0;i<ans.size();i++){
+            cout<<ans[i]<<" ";
+        }
+        cout<<endl;    
+    }
+    
+    return 0;
+}
+
+
+// } Driver Code Ends
