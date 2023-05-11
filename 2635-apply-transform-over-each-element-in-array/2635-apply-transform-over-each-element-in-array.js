162df42f51c5d9977854378ea8c9107285b33cf5
Priyanshu Patel <68877780+Priyanshu-bit@users.noreply.github.com>
2023-05-11 12:02:33 +0530

    Time: 60 ms (48.75%), Space: 41.9 MB (59.42%) - LeetHub

--- 2635-apply-transform-over-each-element-in-array/2635-apply-transform-over-each-element-in-array.js
+++ 2635-apply-transform-over-each-element-in-array/2635-apply-transform-over-each-element-in-array.js
@@ -4,9 +4,10 @@
  * @return {number[]}
  */
 var map = function(arr, fn) {
-  var mappedArray = [];
-  for (var i = 0; i < arr.length; i++) {
-    mappedArray.push(fn(arr[i], i));
-  }
-  return mappedArray;
+    
+    var mappedarray= [];
+    for (var i=0;i<arr.length;i++){
+        mappedarray.push(fn (arr[i],i));
+    }
+    return mappedarray;
 };
