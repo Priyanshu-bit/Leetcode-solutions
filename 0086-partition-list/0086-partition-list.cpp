a858bcc4832d70939b52d147ec46cb68d4ad86a1
Priyanshu Patel <68877780+Priyanshu-bit@users.noreply.github.com>
2023-08-16 09:46:17 +0530

    Time: 4 ms (78.21%), Space: 10.4 MB (21.46%) - LeetHub

--- 0086-partition-list/0086-partition-list.cpp
+++ 0086-partition-list/0086-partition-list.cpp
@@ -1,27 +1,27 @@
 class Solution {
 public:
     ListNode* partition(ListNode* head, int x) {
-        ListNode *left = new ListNode(0);
-        ListNode *right = new ListNode(0);
-        
-        ListNode *leftTail = left;
-        ListNode *rightTail = right;
-        
-        while(head != NULL){
-            if(head->val < x){
-                leftTail->next = head;
-                leftTail = leftTail->next;
-            }
-            else{
-                rightTail->next = head;
-                rightTail = rightTail->next;
-            }
-            head = head->next;
-        }
-        
-        leftTail->next = right->next;
-        rightTail->next = NULL;
-        
-        return left->next;
+         if (head == NULL || head->next == NULL)
+        return head;
+    
+ListNode *dummy1= new ListNode(0);
+ListNode  *dummy2= new ListNode(0);
+
+ListNode  *head1= dummy1;
+ListNode *head2 = dummy2;
+ while(head!=NULL)
+{
+    if (head->val <x){
+        head1->next=head;
+        head1=head1->next;
+    } else{
+        head2->next= head;
+        head2=head2->next;
     }
+    head= head->next;
+}
+head1->next= dummy2->next;
+head2->next=NULL;
+return dummy1->next;
+}
 };
