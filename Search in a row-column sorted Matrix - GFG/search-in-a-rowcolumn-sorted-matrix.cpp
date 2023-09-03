3b21d3870e332a86e89f3b5e9564797a757d67c2
Priyanshu Patel <68877780+Priyanshu-bit@users.noreply.github.com>
2023-09-03 20:20:44 +0530

    Added solution - LeetHub

--- Search in a row-column sorted Matrix - GFG/search-in-a-rowcolumn-sorted-matrix.cpp
+++ Search in a row-column sorted Matrix - GFG/search-in-a-rowcolumn-sorted-matrix.cpp
@@ -12,19 +12,18 @@ class Solution
     bool search(vector<vector<int> > matrix, int n, int m, int x) 
     {
         // code here 
-    int i =0;
-    int j = m-1;
-    while (i<n and j>=0){
-        if (matrix[i][j]==x)
-        return 1;
-        else if (matrix[i][j]>x)
-        j--;
-        else
-        i++;
         
+        int i= 0;
+        int j= m-1;
+        while(i<n&&j>=0){
+        
+        if(matrix[i][j]==x) return 1;
+        else if (matrix[i][j]>x) j--;
+        else i++;
+        }
+        return 0;
     }
-    return 0;
-    }
+    
 };
 
 //{ Driver Code Starts.
