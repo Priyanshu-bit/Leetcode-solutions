3461f5ba387dde2db4defd1be6fc0057d7b9c608
Priyanshu Patel <68877780+Priyanshu-bit@users.noreply.github.com>
2023-10-06 23:42:23 +0530

    Added solution - LeetHub

---
+++ Reverse alternate nodes in Link List - GFG/reverse-alternate-nodes-in-link-list.cpp
@@ -0,0 +1,133 @@
+//{ Driver Code Starts
+#include<stdio.h>
+#include<stdlib.h>
+#include<iostream>
+using namespace std;
+/* A linked list node */
+
+
+struct Node
+{
+    int data;
+    struct Node *next;
+    
+    Node(int x){
+        data = x;
+        next = NULL;
+    }
+    
+};
+
+struct Node *start = NULL;
+
+/* Function to print nodes in a given linked list */
+void printList(struct Node *node)
+{
+    while (node != NULL)
+    {
+        printf("%d ", node->data);
+        node = node->next;
+    }
+    printf("\n");
+    
+}
+
+void insert()
+{
+    int n,value;
+    cin>>n;
+    struct Node *temp;
+    for(int i=0;i<n;i++)
+    {
+        cin>>value;
+        if(i==0)
+        {
+            start = new Node(value);
+            temp = start;
+            continue;
+        }
+        else
+        {
+            temp->next = new Node(value);
+            temp = temp->next;
+        }
+    }
+}
+
+
+// } Driver Code Ends
+/*
+  reverse alternate nodes and append at the end
+  The input list will have at least one element  
+  Node is defined as 
+  struct Node
+  {
+      int data;
+      struct Node *next;
+    
+      Node(int x){
+        data = x;
+        next = NULL;
+      }
+    
+   };
+
+*/
+class Solution
+{
+    public:
+    void rearrange(struct Node *odd)
+    {
+        //add code here
+                Node *tmp = NULL;
+        struct Node *start = odd;
+        Node *end = NULL;
+        Node *rev = NULL;
+        
+        if(start->next == NULL)
+            return;
+        
+        while( start != NULL && start->next != NULL){
+            tmp = start->next;
+            start->next = tmp->next;
+            
+            if(rev == NULL){
+                rev = tmp;
+                rev->next = NULL;
+            }
+            else{
+                tmp->next = rev;
+                rev = tmp;
+            }
+            
+            end = start;
+            start = start->next;
+        }
+        
+        if(start != NULL){
+           start->next = rev;
+           rev = start;
+        }
+            
+        end->next = rev;
+    
+    }
+};
+
+
+
+//{ Driver Code Starts.
+int main()
+{
+    int t;
+    cin>>t;
+    while (t--) {
+        insert();
+        Solution ob;
+        ob.rearrange(start);
+        printList(start);
+    }
+    return 0;
+}
+
+// } Driver Code Ends
