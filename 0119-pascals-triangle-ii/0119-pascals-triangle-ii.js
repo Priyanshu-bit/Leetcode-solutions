289f3acfa493597766f75ff65f539af78e3ea5d0
Priyanshu Patel <68877780+Priyanshu-bit@users.noreply.github.com>
2023-10-16 22:42:02 +0530

    Time: 54 ms (40.93%), Space: 41.8 MB (69.20%) - LeetHub

---
+++ 0119-pascals-triangle-ii/0119-pascals-triangle-ii.js
@@ -0,0 +1,11 @@
+/**
+ * @param {number} rowIndex
+ * @return {number[]}
+ */
+var getRow = function(r) {
+    var ans = new Array(r+1)
+    ans[0]=ans[r]=1
+    for(i=1,up=r;i<r;i++,up--)
+        ans[i] = ans[i-1]*up/i
+    return ans
+};
